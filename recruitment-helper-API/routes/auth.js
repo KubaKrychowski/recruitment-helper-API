f3bda040445017bb2bdc685182e38dd808669ccf
Kuba Krychowski <kuba.krychowski@mieszkanie.pl>
2022-10-06 07:44:21 +0200

    added veryfing token

--- recruitment-helper-API/routes/auth.js
+++ recruitment-helper-API/routes/auth.js
@@ -50,39 +50,64 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.post('/login',async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        if(!(email && password)) {
+        if (!(email && password)) {
             request.status(400).send("All inputs are required");
         }
 
         const user = await User.findOne({ where: { email } });
 
-        if(user && (await bcrypt.compare(password, user.password))) {
+        if (user && (await bcrypt.compare(password, user.password))) {
             const token = jwt.sign(
                 { user_id: user._id, email },
                 `${process.env.JWT_SECRET_KEY}`,
                 {
-                  expiresIn: "2h",
+                    expiresIn: "2h",
                 }
-              );
-                
-              user.token = token;
-              await user.save();
+            );
 
-              const result = {
+            user.token = token;
+            await user.save();
+
+            const result = {
                 username: user.login,
                 externalId: user.externalId,
                 token: token,
-              };
+            };
 
-              res.status(200).json(result);
+            res.status(200).json(result);
         } else {
             res.status(400).send("Invalid Credentials");
         }
 
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get('/login/:token', async (req, res) => {
+    try {
+        const user = await User.findOne({ where: { token: req.params.token } });
+
+        if (!user) {
+            res.status(400).send("Invalid Credentials");
+        }
+
+        jwt.verify(req.params.token, `${process.env.JWT_SECRET_KEY}`, async (err, veryfiedToken) => {
+            if (err) {
+                res.status(401).send(err.message);
+            }
+            const result = {
+                username: user.login,
+                externalId: user.externalId,
+                token: user.token,
+            };
+
+            res.status(200).json(result);
+        });
     } catch (err) {
         console.log(err);
     }
